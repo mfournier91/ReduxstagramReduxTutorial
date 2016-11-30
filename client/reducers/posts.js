3c7050e2d40567a02ac8f2347b3f9a02b9a53178
Marc Fournier <marc.fournier91@gmail.com>
2016-11-30 18:24:48 -0500

    Post reducer increments likes with on click dispatch.

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,9 +3,18 @@
 //info about what happened(the action)
 //copy of current state
 function posts(state = [], action) {
-  console.log("The Posts will change");
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      console.log("Incrementing likes");
+      const i = action.index
+      return [
+        ...state.slice(0,i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i+1),
+      ]
+    default:
+      return state;
+  }
 }
 
 export default posts
