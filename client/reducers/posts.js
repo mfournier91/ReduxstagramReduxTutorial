927b49a56dbaed322d4feb20d66f6c4134932f97
Marc Fournier <marc.fournier91@gmail.com>
2016-11-29 19:06:41 -0500

    Setting up our reducers.

---
+++ client/reducers/posts.js
@@ -0,0 +1,10 @@
+//a reducer takes to things
+
+//info about what happened(the action)
+//copy of current state
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts
