ed1ba90eedb10541add58e1352f2ab3b32b1dd2b
Marc Fournier <marc.fournier91@gmail.com>
2016-11-30 21:00:26 -0500

    Comments are displayed.

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -4,10 +4,26 @@ class Comments extends React.Component {
   constructor(props){
     super(props);
   }
+
+  renderComment(comment, i){
+    return (
+      <div className="comment" key={i}>
+        <strong>{comment.user}</strong>
+        {comment.text}
+        <button className="remove-comment">&times;</button>
+      </div>
+    )
+  }
+
   render() {
     return (
-    <div className="comment">
-      I am the comments
+    <div className="comments">
+      {this.props.postComments.map(this.renderComment)}
+      <form ref="commentForm" className="comment-form" >
+        <input type="text" ref="author" placeholder="author"/>
+        <input type="text" ref="comment" placeholder="comment"/>
+        <input type="submit" hidden />
+      </form>
     </div>
   )
   }
